b39d0efd03760a3666097e73d124a6f9a83765be
Arezoo bakhsharab <Arezo.bakhsharab@gmail.com>
2021-01-22 15:57:42 +0330

    functional component

--- src/App.js
+++ src/App.js
@@ -1,100 +1,88 @@
-import React, { Component } from "react";
+import React, {useState } from "react";
 import "./App.css";
 import FirstFormat from "./FirstFormat";
 import DisplayList from "./DisplayList";
 import OurButton from "./OurButton";
 
-class App extends Component {
-  state = {
-    input: "",
-    list: ["arezoo"],
-    show: true,
-    cmBtn:["Edit"],
-  };
+const App=(props)=>{
+const [inputTodo,setInputTodo]=useState([])
+const [List,setList]=useState(["arezoo"])
+const [cmBtn,setcmBtn]=useState(["Edit"])   
+const [show,setShow]=useState('true') 
 
-  changeState = (event, index) => {
-    this.setState({ input: event.target.value });
+ const changeState = (event, index) => {
+    setInputTodo( event.target.value);
   };
-  addText = () => {
-    const newItem = this.state.input;
-    const list1 = [...this.state.list];
+  const addText = () => {
+    const newItem = inputTodo;
+    const list1 = [...List];
     if (newItem !== "") {
-      list1.push(this.state.input);
-      this.setState({
-        list: list1,
-        input: "",
-      });
+      list1.push(inputTodo);
+      setList(list1)
+      setInputTodo("")
+      // this.setState({
+      //   list: list1,
+      //   input: "",
+      // });
     } else {
       alert("You should enter your comment...");
     }
   };
 
-  removeItem = (index) => {
-    const list2 = this.state.list;
-    list2.splice(index, 1);
-    this.setState({
-      list: list2,
-    });
-  };
-
-  showList = () => {
-    const showList = this.state.show;
-    this.setState({ show: !showList });
-  };
-
-  edit = (item, index) => {
-    
-  //   this.setState({ 
-  // input:item ,
-  // cmBtn:"ok"})
- //console.log(item,index)
-    const cm=[...this.state.cmBtn];
-     cm[index]="ok"
-    // console.log("cmBtn1",this.state.cmBtn)
-     //console.log("cm",cm)
-
-    this.setState({
-       input: item,
-       cmBtn: cm
-      });
-  //  console.log("cmBtn2",this.state.cmBtn)
+ const removeItem = (index) => {
    
-    const newItem = this.state.input;
-    const list1 = [...this.state.list];
+    const list2 = [...List];
+    console.log(list2)
+    list2.splice(index, 1);
+    //console.log("list2",list2)
+    setList(list2)
+    
+  };
+
+  const showList = () => {
+    const showList = show;
+    setShow( !showList )
+  };
+
+  const edit = (item, index) => {
+    const cm=[...cmBtn];
+     cm[index]="ok"
+     setInputTodo(item)
+     setcmBtn(cm)
+    // this.setState({
+    //    input: item,
+    //    cmBtn: cm
+    //   });
+    const newItem = inputTodo;
+    const list1 = [...List];
     list1[index] = newItem;
     if (newItem !== "") {
-      this.setState({
-        list: list1,
-        input: "",
-        cmBtn: "Edit",
-      });
-//      console.log("cmBtn4",this.state.cmBtn) 
+      setList(list1)
+      setInputTodo("")
+      setcmBtn("Edit")
     }
   };
-  render() {
     return (
       <div className="appContent">
         <OurButton
-          insert={this.addText}
-          display={this.showList}
-          myBtn={this.state.cmBtn}
+          insert={addText}
+          display={showList}
+          myBtn={cmBtn}
         />
         <FirstFormat
-          click={(event) => this.changeState(event)}
-          newCm={this.state.input}
+          click={(event) => changeState(event)}
+          newCm={inputTodo}
         />
-        {this.state.show ? (
+        {show ? (
           <DisplayList
-            list={this.state.list}
-            Delete={this.removeItem}
-            change={this.edit}
-            content={this.state.cmBtn}
-            myBtn={this.state.cmBtn}
+            list={List}
+            Delete={removeItem}
+            change={edit}
+            content={cmBtn}
+            myBtn={cmBtn}
           />
         ) : null}
       </div>
     );
   }
-}
-
 export default App;
